10b5f92163d04d1504ca562fb5d30c45041de951
Christophe1 <harris.christophe@gmail.com>
2017-12-09 00:58:24 +0000

    actionbar coming up with menu, have not yet added search

--- main/java/com/example/chris/androiftoolbar/MainActivity.java
+++ main/java/com/example/chris/androiftoolbar/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.chris.androiftoolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
     }
+
+    //creates the menu
+    //the 3 dots are made with app:showAsAction="never"
+    //in menu.xml
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+ /*   @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+    }*/
 }
