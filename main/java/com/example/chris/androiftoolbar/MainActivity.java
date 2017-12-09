b0a530ae2aeadb1beb257594d8e555f7e71384cc
Christophe1 <harris.christophe@gmail.com>
2017-12-09 10:18:15 +0000

    searchview is implemented now

--- main/java/com/example/chris/androiftoolbar/MainActivity.java
+++ main/java/com/example/chris/androiftoolbar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chris.androiftoolbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+
+        //MenuItem item = menu.findItem(R.id.search_view);
+        SearchView searchView = (SearchView)findViewById(R.id.search_view);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText){
+                return false;
+            }
+        });
     }
 
     //creates the menu
@@ -24,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+
+
+
         return super.onCreateOptionsMenu(menu);
     }
 
