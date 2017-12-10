bd1142a844f1d26e805c511f50d0bde248fe5bd1
Christophe1 <harris.christophe@gmail.com>
2017-12-10 23:29:02 +0000

    intent to NewContact is working

--- main/java/com/example/chris/androiftoolbar/MainActivity.java
+++ main/java/com/example/chris/androiftoolbar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chris.androiftoolbar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -7,8 +8,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchView searchView;
+    ListView listView;
+
+    String [] listSource = {
+
+            "Harry",
+            "Hermione",
+            "Voldemort",
+            "Ron",
+            "Dumbledore",
+            "Draco",
+            "Crabbe",
+            "Goyle",
+            "Lucius Malfoy",
+            "Snape",
+            "Sprout",
+            "Flitwick",
+            "McGonagall"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +44,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listSource);
+        listView.setAdapter(adapter);
+
 
         //MenuItem item = menu.findItem(R.id.search_view);
-        SearchView searchView = (SearchView)findViewById(R.id.search_view);
+        searchView = (SearchView)findViewById(R.id.search_view);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -28,8 +59,24 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public boolean onQueryTextChange(String newText){
-                return false;
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null && !newText.isEmpty()) {
+                    List<String> listFound = new ArrayList<String>();
+                    for (String item : listSource) {
+                        if (item.contains(newText))
+                            listFound.add(item);
+                    }
+
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listFound);
+                    listView.setAdapter(adapter);
+                }
+                else{
+                    //if search text is null
+                    //return default
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listSource);
+                    listView.setAdapter(adapter);
+                }
+                return true;
             }
         });
     }
@@ -42,13 +89,20 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
 
-
-
         return super.onCreateOptionsMenu(menu);
     }
 
- /*   @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_contact:
+                //start the NewContact class
+                Intent intent = new Intent(MainActivity.this, NewContact.class);
 
-    }*/
+                startActivity(intent);
+                return true;
+        }
+        return false;
+    }
+
 }
---
+++ main/java/com/example/chris/androiftoolbar/NewContact.java
@@ -0,0 +1,10 @@
+package com.example.chris.androiftoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chris on 10/12/2017.
+ */
+
+public class NewContact extends AppCompatActivity {
+}
